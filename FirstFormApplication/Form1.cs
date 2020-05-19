f45cb401578fc0df8ff441e70f374620270018bc
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-05-19 22:45:15 +0900

    초기 커밋.

---
+++ FirstFormApplication/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstFormApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            mybutton.Enabled = true;
+            mybutton.Text = "안녕하세요!";
+            mybutton.Width = 200;
+
+            for(int i=0;i<5;i++)
+            {
+                Button button = new Button();
+                Controls.Add(button);
+                button.Location = new Point(213, 13 + (23 + 3) * i);
+                button.Text = "동적 생성" +i+"번째";
+                button.Width = 100;
+            }
+            
+        }
+    }
+}
